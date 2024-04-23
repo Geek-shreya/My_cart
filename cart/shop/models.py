c9a6ccec2d5eaf63e95f5663239e72cabc75a096
Geek-shreya <gargshreya003@gmail.com>
2024-04-24 01:07:41 +0530

    conatct page

--- cart/shop/models.py
+++ cart/shop/models.py
@@ -6,9 +6,21 @@ class Product(models.Model):
     category = models.CharField(max_length=50 , default="")
     subcategory = models.CharField(max_length=50 , default="")
     price = models.IntegerField(default=0)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=800)
     pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images" , default="")
 
     def __str__(self):
         return self.product_name
+    
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=80 , default="")
+    phone = models.CharField(max_length=80 , default="")
+    desc = models.CharField(max_length=800 , default="")
+
+    def __str__(self):
+        return self.name
+
+    
--- cart/shop/views.py
+++ cart/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 def index(request):
@@ -24,6 +24,14 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    if request.method == "POST":
+        print(request)
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
@@ -32,8 +40,10 @@ def tracker(request):
 def search(request):
     return render(request, 'shop/search.html')
 
-def productView(request):
-    return render(request, 'shop/productView.html')
+def productView(request,myid):
+    product = Product.objects.filter(id=myid)
+
+    return render(request, 'shop/productView.html', {'product': product[0]})
 
 def checkout(request):
     return render(request, 'shop/checkout.html')
