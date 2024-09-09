00cc9105bcda0a5461a6ad90747ce43b56c03b66
Geek-shreya <gargshreya003@gmail.com>
2024-09-09 12:03:28 +0530

    done

--- cart/shop/views.py
+++ cart/shop/views.py
@@ -9,15 +9,40 @@ from django.http import HttpResponseBadRequest
 
 def index(request):
     allProds = []
-    catProds = Product.objects.values('category', 'id')
-    cats = {item['category'] for item in catProds}
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
     for cat in cats:
-        prod = Product.objects.filter(category = cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
-        nslides = n//4 + ceil((n/4) - (n//4))
-        allProds.append([prod, range(1,nslides), nslides])
-    params = { 'allProds' : allProds}
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
+
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
@@ -53,9 +78,6 @@ def tracker(request):
             return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracker.html')
-
-def search(request):
-    return render(request, 'shop/search.html')
 
 def productView(request,myid):
     product = Product.objects.filter(id=myid)
